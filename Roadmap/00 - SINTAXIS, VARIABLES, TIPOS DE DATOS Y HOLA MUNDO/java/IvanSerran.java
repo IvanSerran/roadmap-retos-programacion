76e22ad4ae3d67099414d10032a19913b424004e
IvanSerran <80716460+IvanSerran@users.noreply.github.com>
2024-05-24 12:39:29 +0200

    #00 - Java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/IvanSerran.java
@@ -0,0 +1,35 @@
+/* 1- Crea un comentario en el código y coloca la URL del sitio web oficial del
+  lenguaje de programación que has seleccionado. */
+
+// https://www.java.com/en/
+
+//////////////////////////////////////////////////////////////////////////////
+/* 2- Representa las diferentes sintaxis que existen de crear comentarios
+    en el lenguaje (en una línea, varias...).*/
+
+// Comentario en una línea
+/* Comentarios en 
+múltiples lineas tal como está en el enunciado */
+
+//////////////////////////////////////////////////////////////////////////////
+// 3- Crea una variable (y una constante si el lenguaje lo soporta). 
+
+String texto = "Esto es un texto"; // variable
+static final int = 100; // constante
+
+//////////////////////////////////////////////////////////////////////////////
+/* 4- Crea variables representando todos los tipos de datos primitios
+   del lenguaje (cadenas de texto, enteros, booleanos...). */
+
+int numeroEntero = 5; 
+float numeroDecimal = 2.5f;
+boolean apagado = false;
+String texto = "Ejemplo de texto";
+char caracter = 'h';
+
+//////////////////////////////////////////////////////////////////////////////
+// 5- Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!" 
+
+System.out.println("¡Hola, java!");
+
+
